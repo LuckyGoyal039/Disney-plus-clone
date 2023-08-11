b0e72c7c64c02ecb148a076700d254ffe3c3a3f1
Lucky <goyallucky2020@gmail.com>
2023-08-11 22:33:31 +0530

    firebase configuration, google login, redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,51 +6,91 @@ import OriginalsIcon from "../assets/images/original-icon.svg";
 import MoviesIcon from "../assets/images/movie-icon.svg";
 import SeriesIcon from "../assets/images/series-icon.svg";
 import styles from "../styles/header.module.scss";
+import { loginWithGoogle } from "../context/AuthContext";
+import React from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+
+import {
+  setUserLoginDetails,
+  selectUserName,
+  selectUserPhoto,
+} from "../features/user/userSlice";
+
 const Header = (props) => {
+  const dispatch = useDispatch();
+  // const navigate = useNavigate();
+  const userName = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
+
+  const setUser = (user) => {
+    dispatch(
+      setUserLoginDetails({
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      })
+    );
+  };
+
+  const handleLogin = async () => {
+    try {
+      const result = await loginWithGoogle();
+      setUser(result.user);
+    } catch (error) {
+      // alert(error.message);
+      console.log(error.message);
+    }
+  };
   return (
     <nav className={styles.navBar}>
       <div className={styles.mainLogo}>
         <img src={Logo} alt="Logo" />
       </div>
-      <div className={styles.menuList}>
-        <a href="/home">
-          <img src={HomeIcon} alt="Home" className={styles.menuIcons} />
-          <span>HOME</span>
-        </a>
-        <a href="/search">
-          <img src={SearchIcon} alt="search" className={styles.menuIcons} />
-          <span>SEARCH</span>
-        </a>
-        <a href="/watchlist">
-          <img
-            src={WatchListIcon}
-            alt="watchList"
-            className={styles.menuIcons}
-          />
-          <span>WATCHLIST</span>
-        </a>
-        <a href="/originals">
-          <img
-            src={OriginalsIcon}
-            alt="originals"
-            className={styles.menuIcons}
-          />
-          <span>ORIGINALS</span>
-        </a>
-        <a href="/movies">
-          <img src={MoviesIcon} alt="movies" className={styles.menuIcons} />
-          <span>MOVIES</span>
-        </a>
-        <a href="/series">
-          <img src={SeriesIcon} alt="series" className={styles.menuIcons} />
-          <span>SERIES</span>
-        </a>
-      </div>
-      <div>
-        <a href="/login" className={styles.loginBtn}>
+      {!userName ? (
+        <a className={styles.loginBtn} href="/#" onClick={handleLogin}>
           LOGIN
         </a>
-      </div>
+      ) : (
+        <React.Fragment>
+          <div className={styles.menuList}>
+            <a href="/home">
+              <img src={HomeIcon} alt="Home" className={styles.menuIcons} />
+              <span>HOME</span>
+            </a>
+            <a href="/search">
+              <img src={SearchIcon} alt="search" className={styles.menuIcons} />
+              <span>SEARCH</span>
+            </a>
+            <a href="/watchlist">
+              <img
+                src={WatchListIcon}
+                alt="watchList"
+                className={styles.menuIcons}
+              />
+              <span>WATCHLIST</span>
+            </a>
+            <a href="/originals">
+              <img
+                src={OriginalsIcon}
+                alt="originals"
+                className={styles.menuIcons}
+              />
+              <span>ORIGINALS</span>
+            </a>
+            <a href="/movies">
+              <img src={MoviesIcon} alt="movies" className={styles.menuIcons} />
+              <span>MOVIES</span>
+            </a>
+            <a href="/series">
+              <img src={SeriesIcon} alt="series" className={styles.menuIcons} />
+              <span>SERIES</span>
+            </a>
+          </div>
+          <img src={userPhoto} alt="Profile" className={styles.profilePhoto}/>
+        </React.Fragment>
+      )}
     </nav>
   );
 };
