2a5f3cbe4ae2d90876e42dc400906a2890102a80
Lucky <goyallucky2020@gmail.com>
2023-08-12 18:32:19 +0530

    add sign out functionality, add home component, navigation

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,26 +1,28 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../firebase";
 
-const loginWithGoogle = () => {
-  const provider = new GoogleAuthProvider();
-
-  const result =signInWithPopup(auth, provider)
-    .then((result) => {
-      // const credential = GoogleAuthProvider.credentialFromResult(result);
-      // const token = credential.accessToken;
-      // const user = result.user;
-      // console.log(user);
-      return result;
-    })
-    .catch((error) => {
-      // const errorCode = error.code;
-      // const errorMessage = error.message;
-      // const email = error.customData.email;
-      // const credential = GoogleAuthProvider.credentialFromError(error);
-      // console.log(errorMessage);
-      return error;
-    });
+const loginWithGoogle = async () => {
+  try {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
     return result;
+  } catch (error) {
+    return error;
+  }
 };
 
-export { loginWithGoogle };
+const sighOutUser = () => {
+  return new Promise((resolve, reject) => {
+    signOut(auth)
+      .then(() => {
+        console.log("signout successfully");
+        resolve();
+      })
+      .catch((error) => {
+        console.lo00g(error);
+        reject(error);
+      });
+  });
+};
+
+export { loginWithGoogle, sighOutUser };
